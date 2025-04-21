4c791a9523d0749089dace28045dfee12e88100f
yevhenkushnir1993 <genyakyshnir@gmail.com>
2025-04-21 12:31:00 +0300

    HW11_TestByCategoryUPD

--- tests/FilterByCategory.spec.ts
+++ tests/FilterByCategory.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/home.page';
+import { CATEGORIES, POWER_TOOLS } from '../pages/category_enum';
 
-  test('Verify user can filter products by category', async ({ page }) => {
+test('Verify user can filter products by category', async ({ page }) => {
     const homePage = new HomePage(page);
-    
     await homePage.homePageNavigate();
-    await page.getByRole('checkbox', { name: 'Sander' }).check();
+    await page.getByRole('checkbox', { name: POWER_TOOLS.SANDER }).check();
     await page.waitForTimeout(3000);
     const productNames = await homePage.getAllProductNames();
-    const allContainSander = productNames.every(name => name.trim().includes('Sander'));
-    expect(allContainSander).toBe(true); 
-  });
+    const allContainSander = productNames.every(name => name.trim().includes(POWER_TOOLS.SANDER));
+    expect(allContainSander).toBe(true);
+}); 
