899d352f50c621031b3e83c802b7d75f392a386c
YevhenKushnir <72440205+YevhenKushnir@users.noreply.github.com>
2025-05-01 14:31:38 +0300

    Update env.config.ts
    Co-authored-by: Eugene Petrik <eugeniy.petrik@gmail.com>

--- env.config.ts
+++ env.config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const config = {
-    weburl: process.env.WEB_URL!,
+    WEB_URL: process.env.WEB_URL!,
     USER_EMAIL: process.env.USER_EMAIL,
     USER_PASSWORD: process.env.USER_PASSWORD,
     USER_NAME: process.env.USER_NAME
